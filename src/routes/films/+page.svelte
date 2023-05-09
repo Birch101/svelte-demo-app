a8812678d435a1b7873785ac1b0e8b326aff9e3b
Chris <christopher.r.birch@gmail.com>
2023-05-09 08:37:45 +0100

    Implemented display of read-only details.

--- src/routes/films/+page.svelte
+++ src/routes/films/+page.svelte
@@ -7,6 +7,7 @@
 	let films = [];
 
 	onMount(() => {
+		// TODO: ideally this url would be in config
 		fetch('https://rest-api-demo-cb.azurewebsites.net/films')
 			.then((response) => response.json())
 			.then((result) => (films = result));
@@ -14,22 +15,26 @@
 </script>
 
 <svelte:head>
-	<title>Films</title>
-	<meta name="description" content="These are my projects" />
+	<title>TV & Films</title>
+	<meta name="description" content="This is a list of all the tv series and films" />
 </svelte:head>
 
 <div class="text-column">
-	<h1>Films</h1>
+	<h1>TV & Films</h1>
 </div>
 
 <div class="container d-flex flex-wrap align-items-center">
 	{#each films as { id, title, plot }, i}
 		<div class="card film-card">
-			<div class="card-body">
-				<h5 class="card-title">{title}</h5>
-				<p class="card-text">{plot}</p>
-				<a href="films/{id}" class="btn btn-primary">View Details</a>
+			<div class="card-header film-card-header">
+				<h5>{title}</h5>
 			</div>
+			<div class="card-body">
+				<p class="card-text">{plot}</p>
+			</div>
+			<div class="card-footer text-end">
+				<a href="films/{id}" class="btn btn-primary">View Details</a>
+            </div>
 		</div>
 	{/each}
 </div>
@@ -38,7 +43,7 @@
 	.film-card {
 		margin: 0.5em;
 		width: 18rem;
-		min-height: 9em;
+		min-height: 18em;
 		min-width: 9em;
 	}
 </style>
--- src/routes/films/[slug]/+page.svelte
+++ src/routes/films/[slug]/+page.svelte
@@ -1,15 +1,19 @@
 <script>
 	import { onMount } from 'svelte';
+	import Carousel from 'svelte-carousel';
 
 	/**
-	 * @type {any[]}
+	 * @type {{ title: any; type: any; plot: any; rated: any; year: any; released: string | number | Date; runtimeMinutes: any; genre: any; director: any; writer: any; actors: any; language: any; country: any; awards: any; metascore: any; imdbRating: any; imdbVotes: any; images: any; }}
 	 */
-	let films = [];
+	let film;
 
 	/** @type {import('./$types').PageData} */
 	export let data;
 
+	let carousel;
+
 	onMount(async () => {
+		// TODO: ideally this url would be in config
 		const res = await fetch('https://rest-api-demo-cb.azurewebsites.net/films/search', {
 			method: 'POST',
 			body: JSON.stringify({ filmId: data.filmId }),
@@ -18,125 +22,126 @@
 			}
 		})
 			.then((response) => response.json())
-			.then((result) => (films = result));
+			.then((result) => (film = result[0]));
 	});
 </script>
 
 <svelte:head>
-	<title>Films</title>
-	<meta name="description" content="These are my projects" />
+	<title>{film?.title}</title>
+	<meta name="description" content="This is the details for {film?.title}" />
 </svelte:head>
 
 <div class="text-column">
-	<h1>Films</h1>
+	<h1>{film?.title} [{film?.type}]</h1>
 </div>
 
 <div class="container d-flex flex-wrap align-items-center">
-	{#each films as { id, title, year, rated, released, runtimeMinutes, genre, director, writer, actors, plot, language, country, awards, poster, metascore, imdbRating, imdbVotes, type }, i}
-		<div class="card film-card">
-			<div class="card-body">
-				<h5 class="card-title">{title}</h5>
-
-
-                <div class="input-group mb-3">
-					<div class="input-group-prepend">
-						<span class="input-group-text film-property-prepend">Title</span>
-					</div>
-					<input type="text" class="form-control" value={title} />
+	<div class="card film-card">
+		<div class="card-body">
+			<div class="input-group mb-3">
+				<div class="input-group-prepend">
+					<span class="input-group-text film-property-prepend">Plot</span>
 				</div>
-                <div class="input-group mb-3">
-					<div class="input-group-prepend">
-						<span class="input-group-text film-property-prepend">Plot</span>
-					</div>
-					<textarea rows="3" class="form-control" value={plot} />
-				</div>
-				<div class="input-group mb-3">
-					<div class="input-group-prepend">
-						<span class="input-group-text film-property-prepend">Rated</span>
-					</div>
-					<input type="text" class="form-control" value={rated} />
-				</div>
-                <div class="input-group mb-3">
-					<div class="input-group-prepend">
-						<span class="input-group-text film-property-prepend">Year</span>
-					</div>
-					<input type="text" class="form-control" value={year} />
-				</div>
-                <div class="input-group mb-3">
-					<div class="input-group-prepend">
-						<span class="input-group-text film-property-prepend">Released</span>
-					</div>
-					<input type="text" class="form-control" value={released} />
-				</div>
-                <div class="input-group mb-3">
-					<div class="input-group-prepend">
-						<span class="input-group-text film-property-prepend">Runtime</span>
-					</div>
-					<input type="text" class="form-control" value={runtimeMinutes} />
-				</div>
-                <div class="input-group mb-3">
-					<div class="input-group-prepend">
-						<span class="input-group-text film-property-prepend">Genre</span>
-					</div>
-					<input type="text" class="form-control" value={genre} />
-				</div>
-                <div class="input-group mb-3">
-					<div class="input-group-prepend">
-						<span class="input-group-text film-property-prepend">Director</span>
-					</div>
-					<input type="text" class="form-control" value={director} />
-				</div>
-                <div class="input-group mb-3">
-					<div class="input-group-prepend">
-						<span class="input-group-text film-property-prepend">Writers</span>
-					</div>
-					<textarea rows="3" class="form-control" value={writer} />
-				</div>
-                <div class="input-group mb-3">
-					<div class="input-group-prepend">
-						<span class="input-group-text film-property-prepend">Actors</span>
-					</div>
-					<input type="text" class="form-control" value={actors} />
-				</div>
-                <div class="input-group mb-3">
-					<div class="input-group-prepend">
-						<span class="input-group-text film-property-prepend">Language</span>
-					</div>
-					<input type="text" class="form-control" value={language} />
-				</div>
-                <div class="input-group mb-3">
-					<div class="input-group-prepend">
-						<span class="input-group-text film-property-prepend">Country</span>
-					</div>
-					<input type="text" class="form-control" value={country} />
-				</div>
-                <div class="input-group mb-3">
-					<div class="input-group-prepend">
-						<span class="input-group-text film-property-prepend">Awards</span>
-					</div>
-					<input type="text" class="form-control" value={awards} />
-				</div>
-                <div class="input-group mb-3">
-					<div class="input-group-prepend">
-						<span class="input-group-text film-property-prepend">Metascore</span>
-					</div>
-					<input type="text" class="form-control" value={metascore} />
-				</div>
-                <div class="input-group mb-3">
-					<div class="input-group-prepend">
-						<span class="input-group-text film-property-prepend">IMDb Rating</span>
-					</div>
-					<input type="text" class="form-control" value={imdbRating} />
-				</div>
-                <div class="input-group mb-3">
-					<div class="input-group-prepend">
-						<span class="input-group-text film-property-prepend">IMDb Votes</span>
-					</div>
-					<input type="text" class="form-control" value={imdbVotes} />
-				</div>
+				<textarea rows="3" readonly class="form-control" value={film?.plot} />
 			</div>
+			<div class="input-group mb-3">
+				<div class="input-group-prepend">
+					<span class="input-group-text film-property-prepend">Rated</span>
+				</div>
+				<input type="text" readonly class="form-control" value={film?.rated} />
+			</div>
+			<div class="input-group mb-3">
+				<div class="input-group-prepend">
+					<span class="input-group-text film-property-prepend">Year</span>
+				</div>
+				<input type="text" readonly class="form-control" value={film?.year} />
+			</div>
+			<div class="input-group mb-3">
+				<div class="input-group-prepend">
+					<span class="input-group-text film-property-prepend">Released</span>
+				</div>
+				<input
+					type="text"
+					readonly
+					class="form-control"
+					value={new Date(film?.released).toDateString()}
+				/>
+			</div>
+			<div class="input-group mb-3">
+				<div class="input-group-prepend">
+					<span class="input-group-text film-property-prepend">Runtime</span>
+				</div>
+				<input type="text" readonly class="form-control" value="{film?.runtimeMinutes} mins" />
+			</div>
+			<div class="input-group mb-3">
+				<div class="input-group-prepend">
+					<span class="input-group-text film-property-prepend">Genre</span>
+				</div>
+				<input type="text" readonly class="form-control" value={film?.genre} />
+			</div>
+			<div class="input-group mb-3">
+				<div class="input-group-prepend">
+					<span class="input-group-text film-property-prepend">Director</span>
+				</div>
+				<input type="text" readonly class="form-control" value={film?.director} />
+			</div>
+			<div class="input-group mb-3">
+				<div class="input-group-prepend">
+					<span class="input-group-text film-property-prepend">Writers</span>
+				</div>
+				<textarea rows="3" readonly class="form-control" value={film?.writer} />
+			</div>
+			<div class="input-group mb-3">
+				<div class="input-group-prepend">
+					<span class="input-group-text film-property-prepend">Actors</span>
+				</div>
+				<input type="text" readonly class="form-control" value={film?.actors} />
+			</div>
+			<div class="input-group mb-3">
+				<div class="input-group-prepend">
+					<span class="input-group-text film-property-prepend">Language</span>
+				</div>
+				<input type="text" readonly class="form-control" value={film?.language} />
+			</div>
+			<div class="input-group mb-3">
+				<div class="input-group-prepend">
+					<span class="input-group-text film-property-prepend">Country</span>
+				</div>
+				<input type="text" readonly class="form-control" value={film?.country} />
+			</div>
+			<div class="input-group mb-3">
+				<div class="input-group-prepend">
+					<span class="input-group-text film-property-prepend">Awards</span>
+				</div>
+				<input type="text" readonly class="form-control" value={film?.awards} />
+			</div>
+			<div class="input-group mb-3">
+				<div class="input-group-prepend">
+					<span class="input-group-text film-property-prepend">Metascore</span>
+				</div>
+				<input type="text" readonly class="form-control" value={film?.metascore} />
+			</div>
+			<div class="input-group mb-3">
+				<div class="input-group-prepend">
+					<span class="input-group-text film-property-prepend">IMDb Rating</span>
+				</div>
+				<input type="text" readonly class="form-control" value={film?.imdbRating} />
+			</div>
+			<div class="input-group mb-3">
+				<div class="input-group-prepend">
+					<span class="input-group-text film-property-prepend">IMDb Votes</span>
+				</div>
+				<input type="text" readonly class="form-control" value={film?.imdbVotes} />
+			</div>
+			{#if film !== undefined}
+				<Carousel bind:this={carousel}>
+					{#each film?.images as { imageURL }, i}
+						<img src={imageURL} class="d-block w-100" alt="Film image {i}" />
+					{/each}
+				</Carousel>
+			{/if}
 		</div>
-	{/each}
+	</div>
 </div>
 
 <style>
@@ -147,7 +152,15 @@
 		min-width: 9em;
 	}
 
-    .film-property-prepend {
-        min-width: 7em;
-    }
+	.film-property-prepend {
+		min-width: 7em;
+	}
+
+	input[readonly] {
+		background-color: white;
+	}
+
+	textarea[readonly] {
+		background-color: white;
+	}
 </style>
